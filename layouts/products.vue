279200cb93b605ee089dc3ae00931c7ac16faccf
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2023-01-12 07:33:24 +0200

    add layouts

---
+++ layouts/products.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/products">products</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <!--output the page content  -->
+    <div>
+      <slot />
+    </div>
+
+    <footer>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/products">Products</NuxtLink></li>
+      </ul>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+  color: #000957;
+}
+</style>
