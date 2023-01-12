852e7516d7cc2cc64619dd1a213e3f15810c3d7d
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2023-01-12 18:55:16 +0200

    error pages

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -11,6 +11,14 @@ const url = "https://fakestoreapi.com/products/" + id;
 // fetch the product
 const { data: product } = await useFetch(url, { key: id });
 
+if (!product.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: "Product not found",
+    fatal: true,
+  });
+}
+
 definePageMeta({
   layout: "products",
 });
