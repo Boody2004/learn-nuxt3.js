4c13ceb31e6d8bc4434a1ee1192c286088adec5c
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2023-01-12 08:16:16 +0200

    add tailwind and some style

--- layouts/products.vue
+++ layouts/products.vue
@@ -1,19 +1,18 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <ul>
-          <li><NuxtLink to="/products">products</NuxtLink></li>
-        </ul>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4 flex justify-between">
+        <NuxtLink to="/" class="font-bold">Nuxt</NuxtLink>
       </nav>
     </header>
+
     <!--output the page content  -->
-    <div>
+    <div class="container mx-auto p-4">
       <slot />
     </div>
 
-    <footer>
-      <ul>
+    <footer class="container mx-auto p-4 flex justify-between border-t-2">
+      <ul class="flex gap-4">
         <li><NuxtLink to="/">Home</NuxtLink></li>
         <li><NuxtLink to="/about">About</NuxtLink></li>
         <li><NuxtLink to="/products">Products</NuxtLink></li>
