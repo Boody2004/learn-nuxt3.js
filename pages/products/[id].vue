19b1e0640a820ee5f905fb502700ebc5f2f14094
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2023-01-12 07:14:49 +0200

    add new pages

---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>
+      LoremDolor do proident laboris ullamco magna amet ad reprehenderit fugiat
+      consectetur dolore fugiat mollit. Cillum in id eiusmod pariatur consequat
+      anim.
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style scoped></style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <p>Products</p>
   </div>
 </template>
